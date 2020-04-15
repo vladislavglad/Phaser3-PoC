fc224ff6660c283958b7de2a82680b453028166d
vlad <vladglad.996@gmail.com>
2020-04-15 17:35:08 -0400

    Reusable MovementManager class.

--- scripts/GameWorld.js
+++ scripts/GameWorld.js
@@ -5,10 +5,15 @@ class GameWorld extends Phaser.Scene {
 
     create() {
         this.add.image(config.width/2, config.height/2, "background-image").setScale(1.3);
-        this.add.sprite(config.width/2 ,config.height/2, "player");
+        this.player = this.physics.add.sprite(config.width/2 ,config.height/2, "player");
+        this.cursors = this.input.keyboard.createCursorKeys();
+
+        //Workaround to reuse code.
+        this.MovementManager = new MovementManager(this.player, this.cursors);
     }
 
     update() {
         //here should go player movement manager that would be imported from a universal module.
+        this.MovementManager.playerMovementManager();
     }
 }
