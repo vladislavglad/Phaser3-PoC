4d082b5072157c425432fcfeaaeb64eb6f405a9d
vladislavglad <vladglad.996@gmail.com>
2020-04-17 19:57:43 -0400

    Enemy now chases after the player.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # Phaser3-PoC
-Testing JS modularity capabilities and "context switching" of content in a single .html file.
+**Testing JS modularity capabilities and "context switching" of content in a single .html file.**
+
+## To run locally.
+### 1. Prerequisites:
+* Node.js installed on your machine.
+* git clone or download this repository.
+
+### 2. In the project directory, you can run:
+#### `npm install`
+Installs all dependencies necessary, namely http-server.
+
+#### `npm start`
+Starts local server on http://127.0.0.1:8080/
--- scripts/GameWorld.js
+++ scripts/GameWorld.js
@@ -10,10 +10,50 @@ class GameWorld extends Phaser.Scene {
 
         //Workaround to reuse code.
         this.MovementManager = new MovementManager(this.player, this.cursors);
+
+        this.physics.world.bounds.width = config.width;
+        this.physics.world.bounds.height = config.height;
+        this.player.setCollideWorldBounds();
+
+        this.enemy = this.physics.add.sprite(20, 20, "baddie", 3);
+        this.speed = 40;
+
+        //Enemy movement engine.
+        this.time.addEvent({
+            delay: 500,
+            callback: this.moveEnemy,
+            callbackScope: this,
+            repeat: Infinity,
+            startAt: 2000,
+        });
     }
 
     update() {
         //here should go player movement manager that would be imported from a universal module.
         this.MovementManager.playerMovementManager();
+    }
+
+    moveEnemy() { 
+        if (this.enemy.active) {
+            let diffX = this.enemy.x - this.player.x;
+            let diffY = this.enemy.y - this.player.y;
+
+            //Move X
+            if (diffX < 0) {
+                this.enemy.scaleX = 1;
+                this.enemy.setVelocityX(this.speed);
+            } else {
+                this.enemy.scaleX = 1;
+                this.enemy.setVelocityX(-this.speed);
+            }
+            //Move Y
+            if (diffY < 0) {
+                this.enemy.scaleY = 1;
+                this.enemy.setVelocityY(this.speed);
+            } else {
+                this.enemy.scaleY = 1;
+                this.enemy.setVelocityY(-this.speed);
+            }
+        }
     }
 }
